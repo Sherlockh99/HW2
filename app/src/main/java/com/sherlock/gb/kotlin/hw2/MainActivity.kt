d6b3bc41658766586607129839c4788151c4000c
Sherlock <sherlockh989@gmail.com>
2022-12-12 18:54:08 +0200

    5b Создать Object. В Object вызвать copy и вывести значения скопированного класса на экран.

--- app/src/main/java/com/sherlock/gb/kotlin/hw2/MainActivity.kt
+++ app/src/main/java/com/sherlock/gb/kotlin/hw2/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.sherlock.gb.kotlin.hw2
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
+    val cat = FirstDataClass("Murzik", 7)
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,10 +21,16 @@ class MainActivity : AppCompatActivity() {
 
         val button2: MaterialButton = findViewById(R.id.key2)
         button2.setOnClickListener {
-            val cat = FirstDataClass("Murzik", 7)
             showToast(cat.toString());
-            showToast(cat.age.toString());
             showToast(cat.name);
+            showToast(cat.age.toString());
+        }
+
+        val buttonCopy: MaterialButton = findViewById(R.id.key_copy)
+        buttonCopy.setOnClickListener {
+            val copy = cat.copy(age = 10, name = "Elephant")
+            showToast(copy.name);
+            showToast(copy.age.toString());
         }
     }
 
